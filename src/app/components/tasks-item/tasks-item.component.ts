4cdf7f96b195451f2ff68629177a1779ce17347c
Santiago <monitocastellani@gmail.com>
2022-06-11 22:25:35 -0300

    hacia el json server

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -10,7 +10,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class TasksItemComponent implements OnInit {
   @Input() task:Task = TASKS[0]
-  faTimes = faTimes
+  faTimes = faTimes;
   constructor() { }
 
   ngOnInit(): void {
